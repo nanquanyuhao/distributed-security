d82956bf2b6a05554a54b380f9ed94fe85862b50
nanquanyuhao <nanquanyuhao@foxmail.com>
2021-06-25 23:25:26 +0800

    权限拦截器编码

---
+++ distributed-security-gateway/src/main/java/net/nanquanyuhao/security/distributed/gateway/filter/AuthFilter.java
@@ -0,0 +1,68 @@
+package net.nanquanyuhao.security.distributed.gateway.filter;
+
+import com.alibaba.fastjson.JSON;
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import net.nanquanyuhao.security.distributed.gateway.common.EncryptUtil;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.OAuth2Request;
+
+import java.util.*;
+
+/**
+ * 权限拦截器
+ */
+public class AuthFilter extends ZuulFilter {
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+
+        RequestContext context = RequestContext.getCurrentContext();
+        // 从安全上下文中获取用户身份对象
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (!(authentication instanceof OAuth2Authentication)) {
+            return null;
+        }
+        OAuth2Authentication oAuth2Authentication = (OAuth2Authentication) authentication;
+        Authentication userAuthentication = oAuth2Authentication.getUserAuthentication();
+        // 取出用户身份信息
+        String principal = userAuthentication.getName();
+
+        // 获取当前用户权限信息
+        List<String> authorities = new ArrayList<>();
+        userAuthentication.getAuthorities().stream().forEach(c -> ((GrantedAuthority) c).getAuthority());
+
+        OAuth2Request oAuth2Request = oAuth2Authentication.getOAuth2Request();
+        Map<String, String> requestParameters = oAuth2Request.getRequestParameters();
+        Map<String, Object> jsonToken = new HashMap<>(requestParameters);
+        if (userAuthentication != null) {
+            jsonToken.put("principal", principal);
+            jsonToken.put("authorities", authorities);
+        }
+
+        // 把身份信息和权限信息放在 json 中，加入 http 的 header 中
+        context.addZuulRequestHeader("json-token",
+                EncryptUtil.encodeUTF8StringBase64(JSON.toJSONString(jsonToken)));
+
+        return null;
+    }
+}
